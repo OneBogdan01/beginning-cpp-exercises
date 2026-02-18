24414756267136973d0933f7f77b6c30587ffdd9
Bogdan Mocanu <bogdan.game.development@gmail.com>
2026-02-18 13:39:23 +0100

    forgot to do 3

--- chapter9/3.cpp
+++ chapter9/3.cpp
@@ -1,12 +1,12 @@
 #include "pch.hpp"
 
-void print_data(const double data[], std::size_t count = 1, std::string_view title = "Data Values",
-                unsigned width = 10, unsigned perLine = 5);
-const double* largest(const double data[], std::size_t count);
-const double* smallest(const double data[], std::size_t count);
-double* shift_range(double data[], std::size_t count, double delta);
-double* scale_range(double data[], std::size_t count, double divisor);
-double* normalize_range(double data[], std::size_t count);
+void print_data(std::span<const double> data, std::size_t count = 1,
+                std::string_view title = "Data Values", unsigned width = 10, unsigned perLine = 5);
+std::optional<double> largest(std::span<const double> data, std::size_t count);
+std::optional<double> smallest(std::span<const double> data, std::size_t count);
+std::span<double> shift_range(std::span<double> data, std::size_t count, double delta);
+std::span<double> scale_range(std::span<double> data, std::size_t count, double divisor);
+std::span<double> normalize_range(std::span<double> data, std::size_t count);
 
 int main() {
     double samples[]{11.0, 23.0, 13.0, 4.0, 57.0, 36.0, 317.0, 88.0, 9.0, 100.0, 121.0, 12.0};
@@ -18,8 +18,8 @@ int main() {
 }
 
 // Prints an array of double values
-void print_data(const double data[], std::size_t count, std::string_view title, unsigned width,
-                unsigned perLine) {
+void print_data(std::span<const double> data, std::size_t count, std::string_view title,
+                unsigned width, unsigned perLine) {
     std::println("{}", title); // Display the title
 
     // Print the data values
@@ -32,37 +32,37 @@ void print_data(const double data[], std::size_t count, std::string_view title, 
     std::println("");
 }
 
-const double* smallest(const double data[], std::size_t count) {
+std::optional<double> smallest(std::span<const double> data, std::size_t count) {
     if (count == 0)
-        return nullptr; // There is no smallest in an empty array
+        return std::nullopt; // There is no smallest in an empty array
 
     std::size_t index_min{};
     for (std::size_t i{1}; i < count; ++i)
         if (data[index_min] > data[i])
             index_min = i;
 
-    return &data[index_min];
+    return data[index_min];
 }
 
-double* shift_range(double data[], std::size_t count, double delta) {
+std::span<double> shift_range(std::span<double> data, std::size_t count, double delta) {
     for (std::size_t i{}; i < count; ++i)
         data[i] += delta;
     return data;
 }
 
-const double* largest(const double data[], std::size_t count) {
+std::optional<double> largest(std::span<const double> data, std::size_t count) {
     if (count == 0)
-        return nullptr; // There is no largest in an empty array
+        return std::nullopt; // There is no largest in an empty array
 
     std::size_t index_max{};
     for (std::size_t i{1}; i < count; ++i)
         if (data[index_max] < data[i])
             index_max = i;
 
-    return &data[index_max];
+    return data[index_max];
 }
 
-double* scale_range(double data[], std::size_t count, double divisor) {
+std::span<double> scale_range(std::span<double> data, std::size_t count, double divisor) {
     if (divisor == 0)
         return data; // Do nothing for a zero divisor
 
@@ -71,7 +71,7 @@ double* scale_range(double data[], std::size_t count, double divisor) {
     return data;
 }
 
-double* normalize_range(double data[], std::size_t count) {
+std::span<double> normalize_range(std::span<double> data, std::size_t count) {
     shift_range(data, count, -(*smallest(data, count)));
     return scale_range(data, count, *largest(data, count));
 }
