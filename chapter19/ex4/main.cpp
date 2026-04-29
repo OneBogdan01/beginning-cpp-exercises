ba630c4fe80a9bcb6ea8b3415115ce199b68be70
Bogdan Mocanu <bogdan.game.development@gmail.com>
2026-04-29 17:23:03 +0200

    chapter 19 done

---
+++ chapter19/ex4/main.cpp
@@ -0,0 +1,45 @@
+﻿#include "pch.hpp"
+
+template <typename T, typename UnaryCondition>
+std::vector<T> collect_all_that_satisfy(const std::vector<T>& vec, UnaryCondition cond) {
+    std::vector<T> result;
+
+    for (auto& element : vec) {
+        if (cond(element)) {
+            result.emplace_back(element);
+        }
+    }
+    return result;
+}
+int main() {
+    {
+        std::vector whole_numbers{1, 2, 3, 5, 10, 200, 5, 1002};
+        std::println("{}", whole_numbers);
+        int user_threshold{};
+        std::cin >> user_threshold;
+        auto above_user_threshold{collect_all_that_satisfy(
+            whole_numbers, [user_threshold](auto element) { return element > user_threshold; })};
+        std::println("{}", above_user_threshold);
+    }
+    {
+        std::vector characters{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'A', 'B', 'C', 'D', 'E'};
+        std::println("{}", characters);
+        auto capital_characters{collect_all_that_satisfy(
+            characters, [](auto element) { return isupper(element) != 0; })};
+        std::println("{}", capital_characters);
+    }
+    {
+        std::vector<std::string> strings{"racecar", "kayak", "cat", "dog", "aca", "jooj"};
+        std::println("{}", strings);
+        auto palindroms{collect_all_that_satisfy(strings, [](std::string_view element) {
+            auto middle_index{(element.length() - 1) / 2};
+            auto first_half{element.substr(0, element.length() / 2)};
+            std::string second_half{element.substr(middle_index + 1, element.length() / 2)};
+            std::reverse(second_half.begin(), second_half.end());
+            auto result{first_half.compare(second_half)};
+            // the same strings so it is a palindrom
+            return result == 0;
+        })};
+        std::println("{}", palindroms);
+    }
+}
---
+++ chapter19/ex5/main.cpp
@@ -0,0 +1,85 @@
+// Using nested classes
+
+#include "truckload.hpp"
+
+#include <exception>
+#include <print>
+
+// See Chapter 12 for an explanation of this function
+
+void logDelivery(const Box& box) {
+    std::println("{}", to_string(box));
+}
+auto createUniformPseudoRandomNumberGenerator(double max)
+
+{
+    std::random_device seeder; // True random number generator to obtain a seed (slow)
+
+    std::default_random_engine generator{seeder()}; // Efficient pseudo-random generator
+
+    std::uniform_real_distribution distribution{1.0, max}; // Generate in [1, max) interval
+
+    return std::bind(distribution, generator); //... and in the darkness bind them!
+}
+
+class DeliveryTruck {
+  public:
+    DeliveryTruck(const Truckload& load) : m_load{load} {};
+    void deliverBox() {
+        auto iterator{m_load.getIterator().getLastBox()};
+        for (auto& delivery : m_deliveries) {
+            delivery(*iterator);
+        }
+        m_load.removeBox(iterator);
+    };
+    void registerOnDelivered(std::function<void(Box&)> func) {
+        m_deliveries.emplace_back(func);
+    };
+
+  private:
+    Truckload m_load;
+    std::vector<std::function<void(Box&)>> m_deliveries;
+};
+int main()
+
+{
+    const double limit{99.0}; // Upper limit on Box dimensions
+
+    auto random{createUniformPseudoRandomNumberGenerator(limit)};
+
+    Truckload load;
+
+    const std::size_t boxCount{20}; // Number of Box object to be created
+
+    // Create boxCount Box objects
+
+    for (std::size_t i{}; i < boxCount; ++i)
+
+        load.addBox(std::make_shared<Box>(random(), random(), random()));
+
+    Truckload truck1{load};
+
+    auto& element = load[0];
+    std::println("{}", to_string(*element));
+
+    try {
+        auto& exception = load[-1];
+    } catch (const std::exception& ex) {
+        std::println("Avoid crash out of bounds {}", ex.what());
+    }
+    std::println("{}", to_string(*element));
+    Truckload truck2{std::move(truck1)};
+
+    truck1 = std::move(load);
+    DeliveryTruck delivery{truck2};
+    size_t counts{};
+    delivery.registerOnDelivered(logDelivery);
+    delivery.registerOnDelivered([&counts](Box& box) {
+        counts += 1;
+        std::println("Total of {} delivered", counts);
+    });
+    delivery.deliverBox();
+    delivery.deliverBox();
+    delivery.deliverBox();
+    delivery.deliverBox();
+}
